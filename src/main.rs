2b12464ca4409a58f0debbc47550d22be5dac5e0
Abhik Jain <abhikjain360@gmail.com>
2021-01-18 20:17:37 +0530

    bump to 0.2

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,91 @@
+#![allow(dead_code, unused_imports)]
+
+#[macro_use]
+extern crate penrose;
+
 use penrose::{
-    bindings::MouseEvent,
     contrib::extensions::Scratchpad,
-    draw::{
-        bar::{widgets::{Workspaces, CurrentLayout, ActiveWindowName, RootWindowName}, StatusBar},
-        Position, TextStyle, XCBDraw,
+    core::{
+        bindings::{KeyEventHandler, MouseEvent},
+        config::Config,
+        data_types::PropVal,
+        helpers::index_selectors,
+        layout::{bottom_stack, side_stack, Layout, LayoutConf},
+        manager::WindowManager,
+        Client, Selector,
     },
-    gen_keybindings, gen_mousebindings,
-    helpers::index_selectors,
-    layout::{bottom_stack, side_stack, Layout, LayoutConf},
-    run_external, run_internal, Backward, Config, Forward, Less, More, Result, WindowManager,
-    XcbConnection,
+    draw::{dwm_bar, TextStyle},
+    logging_error_handler,
+    xcb::{new_xcb_backed_window_manager, XcbDraw, XcbHooks},
+    Backward, Forward, Less, More, PenroseError,
 };
 
-mod bar_text;
-mod colors;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-use colors::*;
+// applications
+const TERMINAL: &str = "st";
+const LAUNCHER: &str = "dmenu_run";
 
+// colors
+const WHITE: u32 = 0xebdbb2ff;
+const GREY: u32 = 0x3c3836ff;
+const BLUE: u32 = 0x458588ff;
+const GREEN: u32 = 0x0055ff33;
+const BLACK: u32 = 0x282828ff;
+
+// bar aesthettics
 const FONT: &str = "Fira Code";
+const HEIGHT: usize = 28;
 
-fn main() -> Result<()> {
-    let mut config = Config::default();
+fn main() {
+    // generate default configs
+    let config = Config::default()
+        .builder()
+        .floating_classes(vec!["dmenu", "polybar"])
+        .gap_px(0)
+        .border_px(5)
+        .focused_border(GREEN)
+        .bar_height(28)
+        .main_ratio_step(0.05)
+        .layouts(vec![
+            Layout::new("[side]", LayoutConf::default(), side_stack, 1, 0.5),
+            Layout::new("[botm]", LayoutConf::default(), bottom_stack, 1, 0.5),
+            Layout::floating("[----]"),
+        ])
+        .build()
+        .expect("unable to build config");
 
-    config.floating_classes = &["dmenu", "polybar"];
-    config.gap_px = 0;
-    config.border_px = 7;
-    config.focused_border = GREEN;
-    config.bar_height = 28;
-    config.main_ratio_step = 0.05;
-    config.layouts = vec![
-        Layout::new("[side]", LayoutConf::default(), side_stack, 1, 0.5),
-        Layout::new("[botm]", LayoutConf::default(), bottom_stack, 1, 0.5),
-    ];
+    let sp = Scratchpad::new(TERMINAL, 0.8, 0.8);
+    let bar = dwm_bar(
+        XcbDraw::new().expect("unable to start xcb draw"),
+        HEIGHT,
+        &TextStyle {
+            font: FONT.to_string(),
+            point_size: 11,
+            fg: WHITE.into(),
+            bg: Some(BLACK.into()),
+            padding: (2.0, 2.0),
+        },
+        BLUE, // highlight
+        GREY, // empty_ws
+        config.workspaces().clone(),
+    )
+    .expect("unable to set the top bar hook");
 
-    let sp = Box::new(Scratchpad::new("st", 0.8, 0.8));
-    let sp_toggle = sp.toggle();
-    let style = &TextStyle {
-        font: FONT.to_string(),
-        point_size: 14,
-        fg: WHITE.into(),
-        bg: Some(BLACK.into()),
-        padding: (2.0, 2.0),
-    };
-    let bar = Box::new(
-        StatusBar::try_new(
-            Box::new(XCBDraw::new()?),
-            Position::Top,
-            28,
-            style.bg.unwrap_or_else(|| 0x000000.into()),
-            &[&style.font],
-            vec![
-                Box::new(Workspaces::new(
-                    &config.workspaces,
-                    style,
-                    BLUE, // highlight
-                    GREY, // empty_ws
-                )),
-                Box::new(CurrentLayout::new(style)),
-                Box::new(ActiveWindowName::new(
-                    &TextStyle {
-                        bg: Some(BLUE.into()),
-                        padding: (6.0, 4.0),
-                        ..style.clone()
-                    },
-                    35,
-                    true,
-                    false,
-                )),
-                Box::new(RootWindowName::new(
-                &TextStyle {
-                    padding: (4.0, 2.0),
-                    ..style.clone()
-                },
-                false,
-                true,
-            )),
-            ],
-        )
-        .unwrap(),
-    );
-    // let bar_toggle = bar.toggle();
-    config.hooks = vec![sp, bar];
+    thread_local! {
+        static BAR_IS_ENABLED: Rc<RefCell<bool>> = Rc::new(RefCell::new(true));
+    }
+
+    // let id = bar.id;
+
+    let hooks: XcbHooks = vec![sp.get_hook(), Box::new(bar)];
 
     let key_bindings = gen_keybindings! {
         // stack management
         "M-d" => run_external!("dmenu_run");
         "M-Return" => run_external!("st");
-        "M-S-Return" => sp_toggle;
+        "M-S-Return" => sp.toggle();
         "M-k" => run_internal!(cycle_client, Backward);
         "M-j" => run_internal!(cycle_client, Forward);
         "M-o" => run_internal!(update_max_main, More);
@@ -99,42 +98,56 @@ fn main() -> Result<()> {
         "M-Tab" => run_internal!(toggle_workspace);
         "M-bracketright" => run_internal!(cycle_screen, Forward);
         "M-bracketleft" => run_internal!(cycle_screen, Backward);
-        "M-S-l" => run_internal!(drag_workspace, Forward);
-        "M-S-j" => run_internal!(drag_workspace, Backward);
         "M-S-q" => run_internal!(exit);
 
         // layouts
         "M-t" => run_internal!(cycle_layout, Forward);
-        "M-b" => Box::new(|wm: &mut WindowManager<'_>| {
 
+        // toggle status bar
+        "M-b" => Box::new(|wm: &mut WindowManager<_>| {
+            let id = match wm.client(&Selector::Condition(&|client: &Client| client.wm_name() == "penrose-statusbar")) {
+                Some(client) => client.id(),
+                None => return Ok(())
+            };
+            if BAR_IS_ENABLED.with(|v| *v.borrow()) {
+                BAR_IS_ENABLED.with(|v| *v.borrow_mut() = false);
+                wm.hide_client(id)
+            } else {
+                BAR_IS_ENABLED.with(|v| *v.borrow_mut() = true);
+                match wm.focus_client(&Selector::WinId(id)) {
+                    Err(_) => Err(PenroseError::MissingClientIds(vec![id])),
+                    Ok(_) => Ok(())
+                }
+            }
         });
 
         // workspaces
         refmap [ config.ws_range() ] in {
-            "M-{}" => focus_workspace [ index_selectors(config.workspaces.len()) ];
-            "M-S-{}" => client_to_workspace [ index_selectors(config.workspaces.len()) ];
+            "M-{}" => focus_workspace [ index_selectors(config.workspaces().len()) ];
+            "M-S-{}" => client_to_workspace [ index_selectors(config.workspaces().len()) ];
         };
 
         // applications
+        "M-S-l" => run_external!("locker");
         "M-w" => run_external!("brave");
         "M-S-w" => run_external!("bookmarks2");
-        "M-S-s" => run_external!("locker");
+        "M-S-d" => run_external!("discord");
         "M-m" => run_external!("st -e ncmpcpp");
         "M-v" => run_external!("st -e alsamixer");
         "M-S-v" => run_external!("pavucontrol");
         "M-e" => run_external!("st -e ranger");
-        "M-p" => run_external!("flameshot gui");
         "M-s" => run_external!("st -e stub");
+        "M-S-p" => run_external!("flameshot gui");
+        "M-p" => run_external!("stup");
     };
 
     let mouse_bindings = gen_mousebindings! {
-        Press Right + [Meta] => |wm: &mut WindowManager, _: &MouseEvent| wm.cycle_workspace(Forward),
-        Press Left + [Meta] => |wm: &mut WindowManager, _: &MouseEvent| wm.cycle_workspace(Backward)
+        Press Right + [Meta] => |wm: &mut WindowManager<_>, _: &MouseEvent| wm.cycle_workspace(Forward),
+        Press Left + [Meta] => |wm: &mut WindowManager<_>, _: &MouseEvent| wm.cycle_workspace(Backward)
     };
 
-    let conn = XcbConnection::new().unwrap();
-    let mut wm = WindowManager::init(config, &conn);
-    wm.grab_keys_and_run(key_bindings, mouse_bindings);
-
-    Ok(())
+    let mut wm = new_xcb_backed_window_manager(config, hooks, logging_error_handler())
+        .expect("unable to create window manager");
+    wm.grab_keys_and_run(key_bindings, mouse_bindings)
+        .expect("unable to start window manager");
 }
